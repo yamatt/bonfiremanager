4b896fc567091ae293c43fb146d5e6bf2b743916
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-10-05 00:45:29 +0100

    Typo in admin module

--- bonfiremanager/admin.py
+++ bonfiremanager/admin.py
@@ -5,4 +5,4 @@ from bonfiremanager import models
 admin.site.register(models.Event)
 admin.site.register(models.Room)
 admin.site.register(models.Talk)
-admin.site.register(models.TimeSlots)
+admin.site.register(models.TimeSlot)
