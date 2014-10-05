061490a74aee52c8ded187fbc841a6221bb4a0be
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-10-05 12:28:43 +0100

    Sync mdoels with etherpad ideas

--- bonfiremanager/models.py
+++ bonfiremanager/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
 class TimeSlot(models.Model):
-    pass
+    event = models.ForeignKey(Event)
+    bookable = models.BooleanField(default=True)
+    end = models.DateTimeField()
+    name = models.CharField(max_length=1024)
+    start = models.DateTimeField()
 
 class Event(models.Model):
-    timeslots = models.ManyToManyField(TimeSlot)
+    name = models.CharField(max_length=1024, unique=True)
+    slug = models.SlugField(max_length=1024)
 
 class Room(models.Model):
     event = models.ForeignKey(Event)
-    name = models.CharField(max_length=1024)
     directions = models.TextField()
+    name = models.CharField(max_length=1024)
 
 class Talk(models.Model):
     room = models.ForeignKey(Room, null=True, blank=True)
-    event = models.ForeignKey(Event)
-    timeslot = models.IntegerField(default=0)
-    title = models.CharField(max_length=1024)
     description = models.TextField()
+    slug = models.SlugField(max_length=1024)
+    timeslot = models.IntegerField(default=0)
+    title = models.CharField(max_length=1024, unique=True)
