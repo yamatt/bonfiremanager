1f40b19a98b08796bb9bc2504fd59d37dfa1e5fa
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-10-05 14:06:49 +0100

    Admin: filter by event

--- bonfiremanager/admin.py
+++ bonfiremanager/admin.py
@@ -7,8 +7,16 @@ class EventAdmin(admin.ModelAdmin):
 
 class TalkAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "room", "timeslot")
+    list_filter = ("room__event",)
+
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_filter = ("event",)
+
+class RoomAdmin(admin.ModelAdmin):
+    list_filter = ("event",)
 
 admin.site.register(models.Event, EventAdmin)
-admin.site.register(models.Room)
+admin.site.register(models.Room, RoomAdmin)
 admin.site.register(models.Talk, TalkAdmin)
-admin.site.register(models.TimeSlot)
+admin.site.register(models.TimeSlot, TimeSlotAdmin)
