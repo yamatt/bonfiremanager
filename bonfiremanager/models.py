d209602169d0e0669bb1daafa082b2325c02b96b
Matt Copperwaite <mattcopp+git@gmail.com>
2014-10-05 12:41:05 +0100

    added useful descriptions for models

--- bonfiremanager/models.py
+++ bonfiremanager/models.py
@@ -1,20 +1,29 @@
 from django.db import models
 
+class Event(models.Model):
+    name = models.CharField(max_length=1024, unique=True)
+    slug = models.SlugField(max_length=1024)
+    
+    def __str__(self):
+        return self.name
+    
 class TimeSlot(models.Model):
     event = models.ForeignKey(Event)
     bookable = models.BooleanField(default=True)
     end = models.DateTimeField()
     name = models.CharField(max_length=1024)
     start = models.DateTimeField()
-
-class Event(models.Model):
-    name = models.CharField(max_length=1024, unique=True)
-    slug = models.SlugField(max_length=1024)
+    
+    def __str__(self):
+        return "{0} ({1})".format(self.name, self.event)
 
 class Room(models.Model):
     event = models.ForeignKey(Event)
     directions = models.TextField()
     name = models.CharField(max_length=1024)
+    
+    def __str__(self):
+        return "{0} ({1})".format(self.name, self.event)
 
 class Talk(models.Model):
     room = models.ForeignKey(Room, null=True, blank=True)
@@ -22,3 +31,6 @@ class Talk(models.Model):
     slug = models.SlugField(max_length=1024)
     timeslot = models.IntegerField(default=0)
     title = models.CharField(max_length=1024, unique=True)
+    
+    def __str__(self):
+        return "{0} in {1}".format(self.title, self.room)
