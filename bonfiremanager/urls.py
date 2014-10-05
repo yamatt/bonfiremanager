ce053fe24bc4c89c48d815edd49c8e330047e6e9
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-10-05 12:32:21 +0100

    Fix views(ish)

--- bonfiremanager/urls.py
+++ bonfiremanager/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'bonfiremanager.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^add$', views.AddTalkView.as_view(), name='talk-add'),
+    url(r'^(?P<event_slug>[a-zA-Z0-9_-]+)$', views.EventView.as_view(), name='event-index'),
+    url(r'^(?P<event_slug>[a-zA-Z0-9_-]+)/add$', views.AddTalkView.as_view(), name='talk-add'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- bonfiremanager/views.py
+++ bonfiremanager/views.py
@@ -2,21 +2,36 @@ from django.views import generic
 
 from bonfiremanager import forms, models
 
+class EventSlugMixin(object):
+    """Mixin that puts the event slug into the template context"""
+    def get_context_data(self, **kwargs):
+        kwargs.setdefault("event_slug", self.kwargs["event_slug"])
+        return super(EventSlugMixin, self).get_context_data(**kwargs)
+
 class IndexView(generic.ListView):
+    model = models.Event
+
+class EventView(EventSlugMixin, generic.ListView):
     model = models.Room
     template_name = "grid.dj.html"
-    event = None
     context_object_name = "rooms"
 
+    def get_queryset(self):
+        qs = super(EventView, self).get_queryset().filter(event__slug=self.kwargs["event_slug"])
+        return qs
+
     def get_event_timeslots(self):
-        return models.TimeSlot.objects.filter(event__id=self.event)
+        return models.TimeSlot.objects.get(event__slug=self.kwargs["event_slug"])
 
     def get_context_data(self, **kwargs):
         kwargs.setdefault("event_timeslots", self.get_event_timeslots)
 
-        return super(IndexView, self).get_context_data(**kwargs)
+        return super(EventView, self).get_context_data(**kwargs)
 
-class AddTalkView(generic.CreateView):
+class AddTalkView(EventSlugMixin, generic.CreateView):
     model = models.Talk
     form_class = forms.TalkForm
     template_name = "talk_form.dj.html"
+
+    def get_initial(self):
+        return {"event": models.Event.objects.get(slug=self.kwargs["event_slug"])}
