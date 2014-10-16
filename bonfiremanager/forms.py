cc619b14ed30da47b82dabedfd8902d872f01e90
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-10-16 01:27:20 +0100

    Users cannot book a talk for an unbookable timeslot
    Fixes #5

--- bonfiremanager/forms.py
+++ bonfiremanager/forms.py
@@ -3,6 +3,10 @@ from django import forms
 from bonfiremanager import models
 
 class TalkForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TalkForm, self).__init__(*args, **kwargs)
+        self.fields["timeslot"].queryset = models.TimeSlot.objects.filter(bookable=True)
+
     class Meta:
         model = models.Talk
         fields = ["title", "description", "timeslot"]
