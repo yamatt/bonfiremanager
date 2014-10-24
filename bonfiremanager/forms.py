0e2dbe7788c578a4fa507a1a384f0af55762d4ff
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-10-24 22:14:13 +0100

    AddTalkForm: Filter out TimeSlots that aren't part of the current Event

--- bonfiremanager/forms.py
+++ bonfiremanager/forms.py
@@ -5,7 +5,7 @@ from bonfiremanager import models
 class TalkForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(TalkForm, self).__init__(*args, **kwargs)
-        self.fields["timeslot"].queryset = models.TimeSlot.objects.filter(bookable=True)
+        self.fields["timeslot"].queryset = models.TimeSlot.objects.filter(event=kwargs["initial"]["event"], bookable=True)
 
     class Meta:
         model = models.Talk
